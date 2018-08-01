875942dc29f1834367ce82373db64d3e35d43913
ProSDLMFDQFK <businessblueox@gmail.com>
2018-08-01 23:37:35 +0200

    Testing..

--- bot.js
+++ bot.js
@@ -178,8 +178,21 @@ client.on('message', message => {
                  ctx.drawImage(ground, 0, 0, 802, 404);
     
      })
-let user = message.mentions.users.first() || message.author
-                                           let url = user.displayAvatarURL.endsWith(".webp") ? user.displayAvatarURL.slice(5, -20) + ".png" : user.displayAvatarURL;
+     let user = message.mentions.users.first();
+     var men = message.mentions.users.first();
+        var heg;
+        if(men) {
+            heg = men
+        } else {
+            heg = message.author
+        }
+      var mentionned = message.mentions.members.first();
+         var h;
+        if(mentionned) {
+            h = mentionned
+        } else {
+            h = message.member
+        }                                           let url = user.displayAvatarURL.endsWith(".webp") ? user.displayAvatarURL.slice(5, -20) + ".png" : user.displayAvatarURL;
                                              jimp.read(url, (err, ava) => {
                                                  if (err) return console.log(err);
                                                  ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
@@ -210,8 +223,7 @@ let user = message.mentions.users.first() || message.author
                                                                     ctx.font = '30px Arial';
                                                      ctx.fontSize = '30px';
                                                      ctx.fillStyle = "#ffffff";
-                                                                             ctx.fillText(`${moment(message.guild.members.get(user.id).joinedAt).fromNow()}`,150, 305);
-                                                              
+                                                                             ctx.fillText(`${moment(h.joinedAt).format('YYYY/M/D HH:mm')} \n \`${moment(h.joinedAt).fromNow()}\``,150, 305);
                                                               
                                                                                                      ctx.font = '30px Arial';
                                                      ctx.fontSize = '30px';
