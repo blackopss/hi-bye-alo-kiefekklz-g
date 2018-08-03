8fc522c6400cf12cca345dbab0c1452d4bb5ee60
Emoko666 <40692364+Emoko666@users.noreply.github.com>
2018-08-03 13:20:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -695,26 +695,4 @@ client.on("message", message => {
 ///end
 
 ///avatar
-client.on('message', message => {
-    if (message.content.startsWith(".avatar")) {
-if(!message.channel.guild) return;
 
-        var mentionned = message.mentions.users.first();
-
-    var client;
-      if(mentionned){
-          var client = mentionned;
-      } else {
-          var client = message.author;
-
-      }
-
-        const embed = new Discord.RichEmbed()
-                           .addField('Requested by:', "<@" + message.author.id + ">")
-
-        .setColor(000000)
-        .setFooter(اسم بوتك, 'ur bot image link')
-        .setImage(${client.avatarURL})
-      message.channel.sendEmbed(embed);
-    }
-});
