279bad3789dd282abba9152f1ef9b580486d723c
ProSDLMFDQFK <businessblueox@gmail.com>
2018-08-03 22:32:40 +0200

    Updated

--- bot.js
+++ bot.js
@@ -838,8 +838,15 @@ client.on('message', message => {
     }
   });
 ///end
-
-
-
-
-
+//Fetch-Invites
+client.on('message', message => {
+    if(message.content.startsWith(`${prefix}invites`)) {
+        message.guild.fetchInvites().then(invs => {
+          let user = message.mentions.users.first() || message.author
+          let personalInvites = invs.filter(i => i.inviter.id === user.id);
+          let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+    message.channel.send(`${user} has ${inviteCount} invites.`);
+    });
+      }
+});
+//end
