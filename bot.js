c6acbdc9b406dbd48b68e3f9cf2c06797f922bdf
Abady <gamersspeaks@gmail.com>
2018-08-06 22:33:31 +0300

    clears

--- bot.js
+++ bot.js
@@ -212,9 +212,7 @@ if (message.content === `${prefix}help`) {
             commandsFound++
             embed.addField(`${commands[cmd].name}`, `**Description:** ${commands[cmd].desc}\n**Usage:** ${prefix + commands[cmd].usage}`);
         }
-
     }
-
     embed.setFooter(`Currently showing user commands. To view another group do ${prefix}help [group / command]`)
     embed.setDescription(`**${commandsFound} commands found** - <> means required, [] means optional`)
 
@@ -227,86 +225,57 @@ if (message.content === `${prefix}help`) {
 } else if (args.join(" ").toUpperCase() === 'GROUPS') {
 
     let groups = '';
-
     for (var cmd in commands) {
         if (!groups.includes(commands[cmd].group)) {
             groups += `${commands[cmd].group}\n`
         }
     }
-
     message.channel.send({embed: {
         description:`**${groups}**`,
         title:"Groups",
         color: 0x1D82B6
     }})
-
     return;
-
-
 } else {
     let groupFound = '';
-
-    for (var cmd in commands) { // This will see if their is a group named after what the user entered.
-
+    for (var cmd in commands) {
         if (args.join(" ").trim().toUpperCase() === commands[cmd].group.toUpperCase()) {
-            groupFound = commands[cmd].group.toUpperCase(); // Lets set the ground found, then break out of the loop.
+            groupFound = commands[cmd].group.toUpperCase();
             break;
         }
-
     }
-
-    if (groupFound != '') { // If a group is found, run this statement.
-
-        // Start of the embed
+    if (groupFound != '') {
         const embed = new RichEmbed()
-            .setColor(0x1D82B6) // You can set this color to whatever you want.
+            .setColor(0x1D82B6)
 
-        // Variables
-        let commandsFound = 0; // We also want to tell them how many commands there are for that specific group.
-
-
-        for (var cmd in commands) { // We can use copy and paste again
-
-            // Checks if the group is "users" - and replace type with group
+        let commandsFound = 0; 
+        for (var cmd in commands) { 
             if (commands[cmd].group.toUpperCase() === groupFound) {
-                // Lets also count commandsFound + 1 every time it finds a command in the group
                 commandsFound++
-                // Lets add the command field to the embed
                 embed.addField(`${commands[cmd].name}`, `**Description:** ${commands[cmd].desc}\n**Usage:** ${prefix + commands[cmd].usage}`); // This will output something like <commandname>[title] [newline] desc: <description> [newline] usage: <usage
             }
 
         }
 
-        // Add some more to the embed - we need to move that out of the for loop.
         embed.setFooter(`Currently showing ${groupFound} commands. To view another group do ${prefix}help [group / command]`)
         embed.setDescription(`**${commandsFound} commands found** - <> means required, [] means optional`)
 
-        // We can output it two ways. 1 - Send to DMs, and tell them that they sent to DMs in chat. 2 - Post commands in chat. [since commands take up a lot let's send to DMs]
         message.author.send({embed})
-        // Post in chat they sent to DMs
         message.channel.send({embed: {
             color: 0x1D82B6,
             description: `**Check your DMs ${message.author}!**`
         }})
-
-        // Make sure you copy and paste into the right place, lets test it now!
-        return; // We want to make sure we return so it doesnt run the rest of the script after it finds a group! Lets test it!
-
-        // Now lets show groups.
+        return; 
     }
-
-    // Although, if a group is not found, lets see if it is a command
-
-    // Variables
     let commandFound = '';
     let commandDesc = '';
     let commandUsage = '';
     let commandGroup = '';
 
-    for (var cmd in commands) { // Copy and paste
+    for (var cmd in commands) { 
 
         if (args.join(" ").trim().toUpperCase() === commands[cmd].name.toUpperCase()) {
-            commandFound = commands[cmd].name; // Lets change this so it doesnt make it go uppcase
+            commandFound = commands[cmd].name; 
             commandDesc = commands[cmd].desc;
             commandUsage = commands[cmd].usage;
             commandGroup = commands[cmd].group;
@@ -315,7 +284,6 @@ if (message.content === `${prefix}help`) {
 
     }
 
-    // Lets post in chat if nothing is found!
     if (commandFound === '') {
         message.channel.send({embed: {
             description:`**No group or command found titled \`${args.join(" ")}\`**`,
@@ -324,7 +292,6 @@ if (message.content === `${prefix}help`) {
 
     }
 
-    // Since this one is smaller, lets send the embed differently.
     message.channel.send({embed: {
         title:'<> means required, [] means optional',
         color: 0x1D82B6,
@@ -333,9 +300,7 @@ if (message.content === `${prefix}help`) {
             value:`**Description:** ${commandDesc}\n**Usage:** ${commandUsage}\n**Group:** ${commandGroup}`
         }]
     }})
-
-    return; // We want to return here so that it doesnt run the rest of the script also.
-
+    return;
 }
 
 }
